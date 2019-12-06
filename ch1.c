ecee129c12f861ab9cec2ee031d392c864bfe8b6
Jefferson Hudson <jefferson.hudson@gmail.com>
2019-12-06 12:09:51 +0000

    Add fahr-celsius function from ch 1.3

--- ch1.c
+++ ch1.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+/* print Fahrenheit-Celsius table
+ *  for fahr = 0, 20, ..., 300 */
+void
+fahr_celsius()
+{
+	float 	fahr,
+	      	celsius;
+	int 	lower,
+		upper,
+		step;
+
+	lower = 0;	// lower limit
+	upper = 300;	// upper limit
+	step  = 20;	// step size
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+
+
+}
+
+int
 main()
 {
 	printf("hello, world!\n");
+	fahr_celsius();
+	return 0;
 }
