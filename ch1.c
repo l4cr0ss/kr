840341e9bccb1873b9e3442c030449b1f5dcf3ca
Jefferson Hudson <jefferson.hudson@gmail.com>
2019-12-13 07:50:38 -0500

    Add ex 1-14

--- ch1.c
+++ ch1.c
@@ -4,12 +4,12 @@
 #define IN  1	/* inside a word */
 #define OUT 0	/* outside a word */
 
-#define WLMAX 32 /* max word size */
+#define WLMAX 255 /* max word size */
 #define WLMOD  1 /* histogram modulus */
 
 /* 1.6 Arrays
  *
- * 	ex 1-13
+ * 	ex 1-14
  * */
 int
 main()
@@ -18,44 +18,29 @@ main()
 	int c,
 		i,			// iterator i
 		j,			// iterator j
-	    nl,			// num letters
-	    nw,			// num words
-	    nc,			// num characters
-		wl,			// word length
-	    state;		// in or out of word
-	int wlens[32];	// histogram
+	    nc;			// num characters
+	int wl[WLMAX];	// histogram
 
 	/* Initializations */
+	nc = 0;
 	for (i = 0; i < WLMAX; i++)
-		wlens[i] = 0;
-	state = OUT;
-	nl = nw = nc = wl = 0;
+		wl[i] = 0;
 
 	/* Calculations */
 	while ((c = getchar()) != EOF) {
 		++nc;
-		if (c == '\n')
-			++nl;
-		if (c == ' ' || c == '\n' || c == '\t') {
-			state = OUT;
-		} else if (state == OUT) {
-			if (wl != 0 && wl < WLMAX)
-				++wlens[wl];
-			wl = 0;
-			state = IN;
-			++nw;
-		} else {
-			++wl;
-		}
+		++wl[c];
 	}
 
 	for (i = 0; i < WLMAX; i++) {
-		if (wlens[i] > 0) {
-			printf("[%2d] (%d) ", i, wlens[i]);
-			for (j = 0; j < wlens[i]; j++) 
+		if (wl[i] > 0) {
+			printf("[%3d] (%3d) ", i, wl[i]);
+			for (j = 0; j < wl[i]; j++) 
 				if (j % WLMOD == 0 )
 					printf("*");
 			printf("\n");
 		}
 	}
 }
+
+// vim:ts=4 sw=4
